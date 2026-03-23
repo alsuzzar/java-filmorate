dff6bffa0310a1ecd55748b6d662256d86ccbb24
Alsu Zaripova <alsuzzar@gmail.com>
2026-03-23 17:29:37 +0100

    checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -10,7 +10,6 @@ import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
 
 @RequiredArgsConstructor
 @RestController
